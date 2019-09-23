17ce40eae7d75864de3c6b92f3eb4eeb102355c4
Tovva Kowalt <bidormo@gmail.com>
2019-09-23 16:02:27 +0000

    it's done

--- up.py
+++ up.py
@@ -1,5 +1,17 @@
 import random
 
 secret = random.randint(1, 101)
-print(f"secret: {secret}"  // TODO: remove upon debug is finished
+print(f"secret: {secret}")  # TODO: remove upon debug is finished
 
+while 1:
+    try:
+        guess = int(input("Enter your guess: "))
+    except ValueError:
+        continue
+    if guess > secret:
+        print("Too big")
+    elif guess < secret:
+        print("Too small")
+    else:
+        print("You win!")
+        break
